974a7c8f298147366e40a6e8eb699559dd195446
Olof Hedén <olof.heden1@gmail.com>
2022-09-29 11:17:44 +0200

    hej

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+localStorage.setItem("Användarnamn", "Hitler");
+localStorage.setItem("lösenord", "heil");
 
 
 
 
+function inputFunction(){
+    let användarNamnInput = document.getElementById("användarNamn").value;
+    let lösenordInput = document.getElementById("lösenOrd").value;
 
- 
- 
+    if (localStorage.användarnamn === användarNamnInput && localStorage.lösenord === lösenordInput){
+        document.getElementById("output").innerHTML = "tjenare";
+    }
+
+    else{
+        document.getElementById("output").innerHTML = "nej";
+    }
+}
